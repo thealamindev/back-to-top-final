644b690c93b4696712cfc2ad7b14e55870d1da8d
Al-Amin <37735466+al-amin-dev@users.noreply.github.com>
2021-06-21 17:52:04 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,16 @@
+//==== Back-to-top button
+  $(window).on('scroll', function(event) {
+    if($(this).scrollTop() > 600){
+        $('.back-to-top').fadeIn(200)
+    } else{
+        $('.back-to-top').fadeOut(200)
+    }
+});
+//==== Animate the scroll to top
+$('.back-to-top').on('click', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: 0,
+    }, 1000);
+});
